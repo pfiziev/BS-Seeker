31347096470935691b6f69137c1b9381273622ff
Petko Fiziev <pf@bender.(none)>
2012-02-16 02:44:51 -0800

    Preprocessing_genome and BS_Seekre_II are made to work with json instead of shelve due to problems with Berkeley DB.
    Preprocessing_genome supports multiple genomes placed in one directory (reference_genome), which is located by default in the same directory as the python scripts (and not in the current working directory as before).
    
    BS_Seeker_II requires a -g option that tells the name of the reference genome. It is looked up by this name in reference_genomes.
    
    BS_Seeker handles correctly the case where the input file is given by its full or relative path.
    
    Lots of code refactoring, too.

--- Preprocessing_genome_II.py
+++ Preprocessing_genome_II.py
@@ -1,208 +1,224 @@
 ﻿import fileinput, string,os, operator, shelve, time, subprocess
+import json
+import re
 from subprocess import Popen
 from optparse import OptionParser
 from utils import *
 
-'''
-#----------------------------------------------------------------
-def reverse_compl_seq(strseq):
-	strseq=strseq.upper()
-	rc_strseq=strseq.translate(string.maketrans("ATCG", "TAGC"))[::-1]
-	return rc_strseq;
-	
-#----------------------------------------------------------------
-'''
 
 if __name__ == '__main__':
 
-	parser = OptionParser()
-	parser.add_option("-f", "--file", dest="filename",help="Input your reference genome file (fasta)", metavar="FILE")
+    parser = OptionParser()
+    parser.add_option("-f", "--file", dest="filename",help="Input your reference genome file (fasta)", metavar="FILE")
 
-	parser.set_defaults(taginfo="Y")
-	parser.add_option("-t", "--tag", dest="taginfo",help="Yes for undirectional lib, no for directional [Y]", metavar="TAG")
+    parser.set_defaults(taginfo="N")
+    parser.add_option("-t", "--tag", dest="taginfo",help="Yes for undirectional lib, no for directional [N]", metavar="TAG")
 
-	parser.set_defaults(bowtiepath = default_bowtie_path)
-	parser.add_option("-p", "--path", dest="bowtiepath",help="Path to Bowtie [~/bowtie-0.10.0/]", metavar="PATH")
+    parser.set_defaults(bowtiepath = default_bowtie_path)
+    parser.add_option("-p", "--path", dest="bowtiepath",help="Path to Bowtie [%s]" % default_bowtie_path, metavar="PATH")
 
-	(options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-	# if no options were given by the user, print help and exit
-	import sys
-	if len(sys.argv) == 1:
-		print parser.print_help()
-		exit(0)
-
-
-
-	fasta_file=options.filename
-	fasta_file=str(fasta_file)
-
-	asktag=options.taginfo
-	asktag=str(asktag)
-	asktag=asktag.upper()
-
-	bowtie_path=options.bowtiepath
-	bowtie_path=str(bowtie_path)
-	if bowtie_path[-1] !="/":
-		bowtie_path=bowtie_path+"/"
-
-	print "Reference genome file: %s"%(fasta_file);
-	print "BS reads from undirectional/directional library: %s"%(asktag);
-	print "Bowtie path: %s"%(bowtie_path);
-	#---------------------------------------------------------------
-	start = time.time()
-
-	os.system('mkdir ./reference_genome')
-	ref_path='./reference_genome/'
-	#---------------------------------------------------------------
-	# 1. First get the complementary genome (also do the reverse)
-	# 2. Then do CT and GA conversions
-	#---------------------------------------------------------------
-	FW_genome={}
-	header=""
-	g=''
-	n=0
-
-	ref_log=open(ref_path+fasta_file+".log","w")
-	refd = shelve.open(ref_path+"refname.shelve",'n')
-
-	for line in fileinput.input(fasta_file):
-		l=line.split()
-		if line[0]!=">":
-			g=g+line[:-1]
-		elif line[0]==">":
-			if header=="":
-				n+=1
-				header=l[0][1:]
-				short_header=str(n).zfill(4)
-			else:
-				g=g.upper()
-				print "reference seq: %s (renamed as %s ) %d bp"%(header,short_header,len(g));
-				ref_log.write("reference seq: %s (renamed as %s ) %d bp"%(header,short_header,len(g))+"\n")
-				refd[short_header]=[header,len(g)]
-				FW_genome[short_header]=g
-
-				g=""
-				header=l[0][1:]
-				n+=1
-				short_header=str(n).zfill(4)
-				
-	g=g.upper()
-	short_header=str(n).zfill(4)
-	print "reference seq: %s (renamed as %s) %d bp"%(header,short_header,len(g));
-	ref_log.write("reference seq: %s (renamed as %s ) %d bp"%(header,short_header,len(g))+"\n")
-	refd[short_header]=[header,len(g)]
-	FW_genome[short_header]=g
-	g=""
-	refd.close()
-	ref_log.close()
-
-	FW_lst=FW_genome.keys()
-	FW_lst.sort()
-
-	#---------------- Python shleve -----------------------------------------------
-	d = shelve.open(ref_path+"ref.shelve",'n')
-	for chr_id in FW_genome:
-		d[chr_id]=FW_genome[chr_id]
-	d.close()
-	#---------------- Reverse complement (Crick strand) ----------------------------
-	header=""
-	RC_genome={}
-	for header in FW_lst:
-		g=FW_genome[header]
-		g=reverse_compl_seq(g)
-		RC_genome[header]=g
-	RC_lst=RC_genome.keys()
-	RC_lst.sort()
+    # if no options were given by the user, print help and exit
+    import sys
+    if len(sys.argv) == 1:
+        print parser.print_help()
+        exit(0)
 
 
-	if asktag=="Y":
-		#---------------- 4 converted fasta -------------------------------------------
-		
-		outf=open(ref_path+'W_C2T.fa','w')
-		for header in FW_lst:
-			outf.write('>%s'%(header)+"\n")
-			g=FW_genome[header]
-			g=g.replace("c","t")
-			g=g.replace("C","T")
-			outf.write('%s'%(g)+'\n')
-		outf.close()
-		print 'end 4-1';
-		
-		outf=open(ref_path+'C_C2T.fa','w')
-		for header in RC_lst:
-			outf.write('>%s'%(header)+"\n")
-			g=RC_genome[header]
-			g=g.replace("c","t")
-			g=g.replace("C","T")
-			outf.write('%s'%(g)+'\n')
-		outf.close()
-		print 'end 4-2';
-		
-		outf=open(ref_path+'W_G2A.fa','w')
-		for header in FW_lst:
-			outf.write('>%s'%(header)+"\n")
-			g=FW_genome[header]
-			g=g.replace("g","a")
-			g=g.replace("G","A")
-			outf.write('%s'%(g)+'\n')
-		outf.close()
-		print 'end 4-3';
-		FW_lst={}
-		
-		outf=open(ref_path+'C_G2A.fa','w')
-		for header in RC_lst:
-			outf.write('>%s'%(header)+"\n")
-			g=RC_genome[header]
-			g=g.replace("g","a")
-			g=g.replace("G","A")
-			outf.write('%s'%(g)+'\n')
-		outf.close()
-		print 'end 4-4';
-		RC_lst={}
-		#---------------- bowtie libraries -------------------------------------------
-		map1=Popen('nohup %sbowtie-build -f ./reference_genome/W_C2T.fa ./reference_genome/W_C2T > ./reference_genome/W_C2T.log'%(bowtie_path),shell=True)
-		map2=Popen('nohup %sbowtie-build -f ./reference_genome/W_G2A.fa ./reference_genome/W_G2A > ./reference_genome/W_G2A.log'%(bowtie_path),shell=True)
-		map3=Popen('nohup %sbowtie-build -f ./reference_genome/C_C2T.fa ./reference_genome/C_C2T > ./reference_genome/C_C2T.log'%(bowtie_path),shell=True)
-		map4=Popen('nohup %sbowtie-build -f ./reference_genome/C_G2A.fa ./reference_genome/C_G2A > ./reference_genome/C_G2A.log'%(bowtie_path),shell=True)
-		map1.wait()
-		map2.wait()
-		map3.wait()
-		map4.wait()
-		os.system('rm ./reference_genome/W_C2T.fa ./reference_genome/W_G2A.fa ./reference_genome/C_C2T.fa ./reference_genome/C_G2A.fa')
-		
-	elif asktag=="N":
-		#---------------- 2 converted fasta -------------------------------------------
-		
-		outf=open(ref_path+'W_C2T.fa','w')
-		for header in FW_lst:
-			outf.write('>%s'%(header)+"\n")
-			g=FW_genome[header]
-			g=g.replace("c","t")
-			g=g.replace("C","T")
-			outf.write('%s'%(g)+'\n')
-		outf.close()
-		print 'end 2-1';
-		FW_lst={}
-		
-		outf=open(ref_path+'C_C2T.fa','w')
-		for header in RC_lst:
-			outf.write('>%s'%(header)+"\n")
-			g=RC_genome[header]
-			g=g.replace("c","t")
-			g=g.replace("C","T")
-			outf.write('%s'%(g)+'\n')
-		outf.close()
-		print 'end 2-2';
-		
-		RC_lst={}
-		#---------------- bowtie libraries -------------------------------------------
-		map1=Popen('nohup %sbowtie-build -f ./reference_genome/W_C2T.fa ./reference_genome/W_C2T > ./reference_genome/W_C2T.log'%(bowtie_path),shell=True)
-		map3=Popen('nohup %sbowtie-build -f ./reference_genome/C_C2T.fa ./reference_genome/C_C2T > ./reference_genome/C_C2T.log'%(bowtie_path),shell=True)
-		map1.wait()
-		map3.wait()
-		os.system('rm ./reference_genome/W_C2T.fa ./reference_genome/C_C2T.fa')	
-	elapsed = (time.time() - start)
-	print ("Time: %s"%(elapsed));
-	print 'END';
+
+    fasta_file=options.filename
+    if fasta_file is None:
+        error('Fasta file for the reference genome must be supported')
+
+    if not os.path.isfile(fasta_file):
+        error('%s cannot be found' % fasta_file)
+
+    asktag=str(options.taginfo).upper()
+
+    if asktag not in 'YN':
+        error('-t option should be either Y or N, not %s' % asktag)
+
+
+    bowtie_path = options.bowtiepath
+
+    if bowtie_path[-1] != "/":
+        bowtie_path += "/"
+
+    print "Reference genome file: %s" % fasta_file
+    print "BS reads from undirectional/directional library: %s" % asktag
+    print "Bowtie path: %s" % bowtie_path
+    #---------------------------------------------------------------
+
+    ref_path = reference_genome_path
+
+    if os.path.exists(ref_path):
+        if not os.path.isdir(ref_path):
+            error("%s must be a directory" % ref_path)
+    else:
+        os.mkdir(ref_path)
+
+
+    # ref_path is a string that containts the directory where the reference genomes are stored with
+    # the input fasta filename appended
+    ref_path = os.path.join(ref_path,
+                            os.path.split(fasta_file)[1] + '_' + asktag + '_')
+
+    #---------------------------------------------------------------
+    # 1. First get the complementary genome (also do the reverse)
+    # 2. Then do CT and GA conversions
+    #---------------------------------------------------------------
+    FW_genome={}
+    header=""
+    g=''
+    n=0
+
+    ref_log=open(ref_path[:-1] + ".log","w")
+
+#    refd = shelve.open(ref_path + "refname.shelve",'n')
+
+    refd = {}
+
+
+    for line in fileinput.input(fasta_file):
+        l=line.split()
+        if line[0]!=">":
+            g=g+line[:-1]
+        elif line[0]==">":
+            if header=="":
+                n+=1
+                header=l[0][1:]
+                short_header=str(n).zfill(4)
+            else:
+                g=g.upper()
+                print "reference seq: %s (renamed as %s ) %d bp"%(header,short_header,len(g))
+                ref_log.write("reference seq: %s (renamed as %s ) %d bp"%(header,short_header,len(g))+"\n")
+                refd[short_header]=[header,len(g)]
+                FW_genome[short_header]=g
+
+                g=""
+                header=l[0][1:]
+                n+=1
+                short_header=str(n).zfill(4)
+
+    g=g.upper()
+    short_header=str(n).zfill(4)
+    print "reference seq: %s (renamed as %s) %d bp"%(header,short_header,len(g))
+    ref_log.write("reference seq: %s (renamed as %s ) %d bp"%(header,short_header,len(g))+"\n")
+    refd[short_header]=[header,len(g)]
+    FW_genome[short_header]=g
+    g=""
+
+    json.dump(refd, open(ref_path + 'refname.json', 'w'))
+
+#    refd.close()
+    ref_log.close()
+
+    FW_lst=FW_genome.keys()
+    FW_lst.sort()
+
+    #---------------- Python shelve -----------------------------------------------
+    json.dump(FW_genome, open(ref_path + 'ref.json', 'w'))
+
+#    d = shelve.open(ref_path + "ref.shelve",'n')
+#    for chr_id in FW_genome:
+#        d[chr_id]=FW_genome[chr_id]
+#    d.close()
+
+    #---------------- Reverse complement (Crick strand) ----------------------------
+    header=""
+    RC_genome={}
+    for header in FW_lst:
+        g=FW_genome[header]
+        g=reverse_compl_seq(g)
+        RC_genome[header]=g
+    RC_lst=RC_genome.keys()
+    RC_lst.sort()
+
+
+
+    path_dict = {'bowtie_path' : bowtie_path, 'ref_path' : ref_path}
+
+    if asktag=="Y":
+        #---------------- 4 converted fasta -------------------------------------------
+
+        outf=open(ref_path + 'W_C2T.fa','w')
+        for header in FW_lst:
+            outf.write('>%s\n' % header)
+            g=FW_genome[header]
+            g=g.replace("c","t")
+            g=g.replace("C","T")
+            outf.write('%s\n' % g)
+        outf.close()
+        print 'end 4-1'
+
+        outf=open(ref_path + 'C_C2T.fa','w')
+        for header in RC_lst:
+            outf.write('>%s\n'% header)
+            g=RC_genome[header]
+            g=g.replace("c","t")
+            g=g.replace("C","T")
+            outf.write('%s\n'% g)
+        outf.close()
+        print 'end 4-2'
+
+        outf=open(ref_path + 'W_G2A.fa','w')
+        for header in FW_lst:
+            outf.write('>%s\n'% header)
+            g=FW_genome[header]
+            g=g.replace("g","a")
+            g=g.replace("G","A")
+            outf.write('%s\n'% g)
+        outf.close()
+        print 'end 4-3'
+
+        outf=open(ref_path + 'C_G2A.fa','w')
+        for header in RC_lst:
+            outf.write('>%s\n'% header)
+            g=RC_genome[header]
+            g=g.replace("g","a")
+            g=g.replace("G","A")
+            outf.write('%s\n' % g)
+        outf.close()
+        print 'end 4-4'
+        #---------------- bowtie libraries -------------------------------------------
+        to_bowtie = ['W_C2T', 'W_G2A', 'C_C2T', 'C_G2A']
+
+    else: # asktag=="N"
+        #---------------- 2 converted fasta -------------------------------------------
+
+        outf=open(ref_path+'W_C2T.fa','w')
+        for header in FW_lst:
+            outf.write('>%s\n' % header)
+            g=FW_genome[header]
+            g=g.replace("c","t")
+            g=g.replace("C","T")
+            outf.write('%s\n' % g)
+        outf.close()
+        print 'end 2-1'
+        FW_lst={}
+
+        outf=open(ref_path+'C_C2T.fa','w')
+        for header in RC_lst:
+            outf.write('>%s\n'% header)
+            g=RC_genome[header]
+            g=g.replace("c","t")
+            g=g.replace("C","T")
+            outf.write('%s\n'% g)
+        outf.close()
+        print 'end 2-2'
+        to_bowtie = ['W_C2T', 'C_C2T']
+
+
+    # start bowtie-build for all converted genomes and wait for the processes to finish
+    for proc in [Popen('nohup %(bowtie_path)sbowtie-build -f %(fname)s.fa %(fname)s > %(fname)s.log'% {'bowtie_path' : bowtie_path,
+                                                                                                       'fname' : ref_path + fname} ,
+                       shell=True) for fname in to_bowtie]:
+        proc.wait()
+
+    # delete fasta files of converted genomes
+    os.system('rm -rf ' + ' '.join(ref_path + fname + '.fa' for fname in to_bowtie))
+
+
+    elapsed('Done')
 
--- utils.py
+++ utils.py
@@ -1,74 +1,87 @@
-import string, os
+import os
 
 #----------------------------------------------------------------
+import datetime
+
+
 def reverse_compl_seq(strseq):
-	strseq=strseq.upper()
-	rc_strseq=strseq.translate(string.maketrans("ATCG", "TAGC"))[::-1]
-	return rc_strseq;
-	
-def compl_seq(strseq):
-	strseq=strseq.upper()
-	c_strseq=strseq.translate(string.maketrans("ATCG", "TAGC"))
-	return c_strseq;
-	
+    rc_dict = {'A' : 'T', 'T' : 'A', 'G' : 'C', 'C' : 'G'}
+    return ''.join(rc_dict.get(c, c) for c in reversed(strseq.upper()))
+
+
 def N_MIS(r,g):
-	combo=[]
-	if len(r)==len(g):
-		combo=[r[i]+g[i] for i in range(len(r)) if r[i]!=g[i] and r[i] !="N" and g[i]!="N"]
-		combo=[x for x in combo if x !="TC"]
-	return len(combo);
-	
+    combo=[]
+    if len(r)==len(g):
+        combo=[r[i]+g[i] for i in range(len(r)) if r[i]!=g[i] and r[i] !="N" and g[i]!="N"]
+        combo=[x for x in combo if x !="TC"]
+    return len(combo)
+
 #----------------------------------------------------------------
 
 def methy_seq(r,g_long):
-	H=['A','C','T']
-	g_long=g_long.replace("_","")
-	m_seq=str()
-	xx="-"
-	for i in range(len(r)):
-		if r[i] not in ['C','T']:
-			xx="-"
-		elif r[i]=="T" and g_long[i+2]=="C": #(unmethylated):
-			if g_long[i+3]=="G":
-				xx="x"
-			elif g_long[i+3] in H :
-				if g_long[i+4]=="G":
-					xx="y"
-				elif g_long[i+4] in H :
-					xx="z"
+    H=['A','C','T']
+    g_long=g_long.replace("_","")
+    m_seq=str()
+    xx="-"
+    for i in range(len(r)):
+        if r[i] not in ['C','T']:
+            xx="-"
+        elif r[i]=="T" and g_long[i+2]=="C": #(unmethylated):
+            if g_long[i+3]=="G":
+                xx="x"
+            elif g_long[i+3] in H :
+                if g_long[i+4]=="G":
+                    xx="y"
+                elif g_long[i+4] in H :
+                    xx="z"
 
-		elif r[i]=="C" and g_long[i+2]=="C": #(methylated):
-			if g_long[i+3]=="G":
-				xx="X"
-			elif g_long[i+3] in H :
-				if g_long[i+4]=="G":
-					xx="Y"
-				elif g_long[i+4] in H :
-					xx="Z"
-		else:
-			xx="-"			
-					
-		m_seq=m_seq+xx
-	return m_seq;
+        elif r[i]=="C" and g_long[i+2]=="C": #(methylated):
+            if g_long[i+3]=="G":
+                xx="X"
+            elif g_long[i+3] in H :
+                if g_long[i+4]=="G":
+                    xx="Y"
+                elif g_long[i+4] in H :
+                    xx="Z"
+        else:
+            xx="-"
+
+        m_seq += xx
+    return m_seq
 
 
 
 def mcounts(mseq,mlst,ulst):
-	out_mlst=[mlst[0]+mseq.count("X"),mlst[1]+mseq.count("Y"),mlst[2]+mseq.count("Z")]
-	out_ulst=[ulst[0]+mseq.count("x"),ulst[1]+mseq.count("y"),ulst[2]+mseq.count("z")]
-	return out_mlst,out_ulst;
+    out_mlst=[mlst[0]+mseq.count("X"),mlst[1]+mseq.count("Y"),mlst[2]+mseq.count("Z")]
+    out_ulst=[ulst[0]+mseq.count("x"),ulst[1]+mseq.count("y"),ulst[2]+mseq.count("z")]
+    return out_mlst,out_ulst
 
 #-------------------------------------------------------------------------------------
 
 # set a reasonable defaults
 def find_location(program):
-	def is_exe(fpath):
-		return os.path.exists(fpath) and os.access(fpath, os.X_OK)
-	for path in os.environ["PATH"].split(os.pathsep):	
-		if is_exe(os.path.join(path, program)):
-			return path
+    def is_exe(fpath):
+        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
+    for path in os.environ["PATH"].split(os.pathsep):
+        if is_exe(os.path.join(path, program)):
+            return path
 
-	return None
-    
+    return None
+
 default_bowtie_path = find_location('bowtie') or "~/bowtie-0.12.7/"
 
+reference_genome_path = os.path.join(os.path.split(globals()['__file__'])[0],'reference_genome')
+
+
+
+def error(msg):
+    print 'ERROR: %s' % msg
+    exit(1)
+
+
+global_stime = datetime.datetime.now()
+def elapsed(msg = None):
+    print "[%s]" % msg if msg is not None else "+", "Last:" , datetime.datetime.now() - elapsed.stime, '\tTotal:', datetime.datetime.now() - global_stime
+    elapsed.stime = datetime.datetime.now()
+
+elapsed.stime = datetime.datetime.now()
